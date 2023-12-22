bfdbe9b784de6a9a0d77d9ac14ece69d151f78a8
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2023-12-22 17:33:42 +0200

    Create User class

--- src/main/java/com/CSED26/SpeedMail/User.java
+++ src/main/java/com/CSED26/SpeedMail/User.java
@@ -18,6 +18,24 @@ public class User {
         return allMessage;
     }
 
+    private String getPassword() {
+        return password;
+    }
+
+    public static boolean checkPassword(String address, String password){
+        User user = User.getUser(address);
+        if(password.equals(user.getPassword()))
+            return true;
+        return false;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public String getAdress() {
+        return adress;
+    }
+    
     public static User getUser(String address) {
         return null;
     }
