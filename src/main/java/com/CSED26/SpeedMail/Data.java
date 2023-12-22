ada20cb7de8f407365d06250bfc7080f08382413
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2023-12-22 22:12:41 +0200

    Data class done

--- src/main/java/com/CSED26/SpeedMail/Data.java
+++ src/main/java/com/CSED26/SpeedMail/Data.java
@@ -8,18 +8,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Data {
 
+    private Data(){
+        throw new IllegalStateException("Data class");
+    }
+
+    private static String mailsPath = "Data/Mails/";
+    private static String usersPath = "Data/Users/";
+    private static String foldersPath = "Data/Folders/";
     //  Mails Data
     public static void saveMail(String id, Mail mail) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("Data/Mails/" + id);
+        File file = new File( mailsPath + id);
         objectMapper.writeValue(file, mail);
-        System.out.println("Shapes successfully written to JSON file.");
+        System.out.println("Mail successfully written to JSON file.");
     }
 
     public static Mail getMail(String id) throws IOException {
         Mail mail;
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("Data/Mails/" + id);
+        File file = new File(mailsPath + id);
 
         // Read JSON file
         mail = objectMapper.readValue(file, Mail.class);
@@ -29,7 +36,7 @@ public class Data {
     // Users Data
     public static void saveUser(String address, User user) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("Data/Users/" + address);
+        File file = new File(usersPath + address);
         objectMapper.writeValue(file, user);
         System.out.println("Shapes successfully written to JSON file.");
     }
@@ -37,7 +44,7 @@ public class Data {
     public static User getUser(String address) throws IOException {
         User user;
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("Data/Users/" + address);
+        File file = new File(usersPath + address);
 
         // Read JSON file
         user = objectMapper.readValue(file, User.class);
@@ -47,7 +54,7 @@ public class Data {
     //  Folders Data
     public static void saveFolder(String id, Folder folder) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("Data/Mails/" + id);
+        File file = new File(foldersPath + id);
         objectMapper.writeValue(file, folder);
         System.out.println("Shapes successfully written to JSON file.");
     }
@@ -55,7 +62,7 @@ public class Data {
     public static Folder getFolder(String id) throws IOException {
         Folder folder;
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("Data/Mails/" + id);
+        File file = new File(foldersPath + id);
 
         // Read JSON file
         folder = objectMapper.readValue(file, Folder.class);
